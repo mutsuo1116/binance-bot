8d3e38137f1a2f6769d0707be072ed27e9387af5
mutsuo1116 <mirasimanbekov500@gmail.com>
2026-09-15 09:53:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -776,6 +776,13 @@ class BinanceClient:
 
         class BinanceClient:
 
+    def place_order(self, params):
+        return self.signed_request(
+            "POST",
+            "/fapi/v1/order",
+            params
+        )
+
     def place_algo_order(self, params):
         params = dict(params)
 
